16d8e88fe1ae5a35e4fe3fe0b7b26f1d6ad48efd
zhaohu14 <1625469743@qq.com>
2023-03-14 17:10:54 +0800

    first commit

--- README.md
+++ README.md
@@ -17,3 +17,10 @@ npm run build
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### 出现Syntax Error: TypeError: loaderContext.getOptions is not a function
+```
+降低 cli-plugin-typescript版本
+
+npm i -D @vue/cli-plugin-typescript@4.5.15 
+```
---
+++ src/pages/me/me.vue
@@ -0,0 +1,38 @@
+<template>
+  <view class="content">
+      <view>
+          <text class="title">{{ title }}</text>
+      </view>
+  </view>
+</template>
+
+<script lang="ts">
+  import {
+      defineComponent
+  } from "vue";
+  export default defineComponent({
+      setup() {
+          return {
+              title: "我的"
+          }
+      }
+  })
+</script>
+
+<style>
+  .content {
+      text-align: center;
+      height: 400upx;
+  }
+
+  .logo {
+      height: 200upx;
+      width: 200upx;
+      margin-top: 200upx;
+  }
+
+  .title {
+      font-size: 36upx;
+      color: #8f8f94;
+  }
+</style>
